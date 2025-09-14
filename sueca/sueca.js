b6be0f527b972db6de507db7e2ec9a52a269e4ad
Serginho <antoniosergio0606@gmail.com>
2025-09-14 12:19:43 +0100

    add bandeira effect

--- sueca/sueca.js
+++ sueca/sueca.js
@@ -117,13 +117,13 @@ sueca = function () {
         const isLastPointOfSet = score.type !== 'unlimited'
             ? currentPosition % 4 === 0
             : currentPosition % score.unlimitedQty === 0;
-        if (isMultiple === player && !isLastPointOfSet) {
-            score['score' + player][currentPosition] = score['score' + player][currentPosition - 1] + 1;
-            score['score' + player][currentPosition - 1] = false;
-
-            if (score.type !== 'unlimited' && currentPosition % 4 === 3) {
-                // Final point of the game (normal mode)
-                isMultiple = false;
+        if (isMultiple === player) {
+            if (isLastPointOfSet) {
+                console.log(score['score' + player]);
+                score['score' + player][currentPosition-1]++;
+            } else {
+                score['score' + player][currentPosition] = score['score' + player][currentPosition - 1] + 1;
+                score['score' + player][currentPosition - 1] = false;
             }
 
             // Can't have 3 point multiples, but we must wait for the possible fourth
@@ -220,15 +220,15 @@ sueca = function () {
         let index = 0;
         while (index < scoreUp.length) {
             html += `
-<div class="board">
-    <div class="players">
-        <div class="player" data-renamable="1">${score.player1 || 'N'}</div>
-        <div class="divider divider--horizontal"></div>
-        <div class="player" data-renamable="2">${score.player2 || 'V'}</div>
-    </div>
-    <div class="divider"></div>
-    ${buildFromScores(scoreUp.slice(index, index + chunkSize), scoreDown.slice(index, index + chunkSize), score.type)}
-</div>`;
+                <div class="board">
+                    <div class="players">
+                        <div class="player" data-renamable="1">${score.player1 || 'N'}</div>
+                        <div class="divider divider--horizontal"></div>
+                        <div class="player" data-renamable="2">${score.player2 || 'V'}</div>
+                    </div>
+                    <div class="divider"></div>
+                    ${buildFromScores(scoreUp.slice(index, index + chunkSize), scoreDown.slice(index, index + chunkSize), score.type)}
+                </div>`;
             index += chunkSize;
         }
 
@@ -237,36 +237,44 @@ sueca = function () {
         return html;
     }
 
-    const buildUnit = (up, down) => {
+    const buildUnit = (up, down, addBandeira) => {
         return `
-<div class="unit">
-    <div class="point" data-player="1"><div class="${getPointClasses(up)}"></div></div>
-    <div class="divider" data-player="1"></div>
-    <div class="divider divider--horizontal"></div>
-    <div class="divider" data-player="2"></div>
-    <div class="point" data-player="2"><div class="${getPointClasses(down)}"></div></div>
-</div>`;
+            <div class="unit">
+                <div class="point" data-player="1"><div class="${getPointClasses(up)}">${up && addBandeira && buildBandeira('up') || ''}</div></div>
+                <div class="divider" data-player="1"></div>
+                <div class="divider divider--horizontal"></div>
+                <div class="divider" data-player="2"></div>
+                <div class="point" data-player="2"><div class="${getPointClasses(down)}">${down && addBandeira && buildBandeira('down') || ''}</div></div>
+            </div>`;
     }
 
     const buildEmptyUnit = () => {
         return `<div class="unit"><div class="divider divider--horizontal"></div></div>`;
     }
 
-    const buildWinUnit = (position) => {
+    const buildWinUnit = (position, addBandeira) => {
         return `
-<div class="unit">
-    <div class="divider divider--horizontal"></div>
-    <div class="win win--${position}" data-player="${position === 'up' ? 1 : 2}"></div>
-</div>`;
+            <div class="unit">
+                <div class="divider divider--horizontal"></div>
+                <div class="win win--${position}" data-player="${position === 'up' ? 1 : 2}">
+                    ${addBandeira && buildBandeira(position) || ''}
+                </div>
+            </div>`;
     }
 
     const buildEndUnit = () => {
         return `
-<div class="unit">
-    <span class="game-separator">\\/</span>
-<div class="divider divider--horizontal"></div>
-    <span class="game-separator">/\\</span>
-</div>`;
+            <div class="unit">
+                <span class="game-separator">\\/</span>
+            <div class="divider divider--horizontal"></div>
+                <span class="game-separator">/\\</span>
+            </div>`;
+    }
+
+    const buildBandeira = (position) => {
+        return `<div class="bandeira bandeira--${position} bandeira__pole">
+                    <div class="bandeira__flag wave"></div>
+                </div>`;
     }
 
     /* Drawing Logic */
@@ -277,21 +285,27 @@ sueca = function () {
         let scoreDownTotal = 0;
         for (let i = 0; i < scoreUp.length; i++) {
             if (type === 'unlimited') {
-                scoreUpTotal += scoreUp[i] ? 1 : 0;
-                scoreDownTotal += scoreDown[i] ? 1 : 0;
-                html += buildUnit(scoreUp[i], scoreDown[i]);
-                if (scoreUpTotal === unlimitedQty || scoreDownTotal === unlimitedQty) {
+                scoreUpTotal += scoreUp[i];
+                scoreDownTotal += scoreDown[i];
+                if (scoreUpTotal >= unlimitedQty || scoreDownTotal >= unlimitedQty) {
+                    if (scoreUp.slice(i-3, i+1).reduce((a, b) => a + b, 0) > 4 || scoreDown.slice(i-3, i+1).reduce((a, b) => a + b, 0) > 4) {
+                        html += buildUnit(!!scoreUp[i], !!scoreDown[i], true);
+                    } else {
+                        html += buildUnit(scoreUp[i], scoreDown[i]);
+                    }
                     scoreUpTotal = scoreDownTotal = 0;
                     html += buildEndUnit();
+                } else{
+                    html += buildUnit(scoreUp[i], scoreDown[i]);
                 }
             } else if ((i + 1) % 4 === 0) {
                 // every fourth point means a game is over
                 if (scoreUp[i]) {
                     scoreUpTotal++;
-                    html += buildWinUnit('up');
+                    html += buildWinUnit('up', scoreUp[i] === 4);
                 } else if (scoreDown[i]) {
                     scoreDownTotal++;
-                    html += buildWinUnit('down');
+                    html += buildWinUnit('down', scoreDown[i] === 4);
                 } else {
                     html += buildEmptyUnit();
                 }
