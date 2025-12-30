f014c716a377e42ac419fc63fdee9e415d46bdca
Serginho <antoniosergio0606@gmail.com>
2025-12-30 17:57:18 +0000

    Mimic - How to play

--- mimic/mimicSettings.js
+++ mimic/mimicSettings.js
@@ -632,7 +632,7 @@ const mimicSettings = {
     identifier: 'mimic',
     title: 'Word Game',
     newScore: `
-<select id="language" class="absolute absolute--secondish">${Object.keys(words).map(lang => `<option value="${lang}">${lang}</option>`).join('')}</select>
+<select id="language" class="absolute absolute--thirdish">${Object.keys(words).map(lang => `<option value="${lang}">${lang}</option>`).join('')}</select>
 <table id="board" class="board board--table"></table>
 <div class="section mb-1 w-full">
     <div class="section section--row w-full">
@@ -642,6 +642,16 @@ const mimicSettings = {
     </div>
 </div>
 `,
+    howToPlay: JSON.stringify({
+        intro: 'Neste jogo de mimica, cada jogador tem 1 minuto para fazer mímica do máximo numero de palavras possível.',
+        rules: [
+            'Escolhe um tema, o numero e nome das equipas e começa a jogar.',
+            'Se a equipa acertar na palavra, clica em "Confirm" e ganham um ponto',
+            'Se a equipa, ou o jogador não conseguirem acertar podem perder um ponto para saltar a palavra clicando no botão "Skip"',
+            'Ao fim de um minuto, devem passar a vez à equipa adversária',
+        ],
+        conclusion: 'Podem jogar tantas vezes quantas quiserem. No fim ganha a equipa com mais pontos.',
+    }),
 };
 
 export default mimicSettings;
